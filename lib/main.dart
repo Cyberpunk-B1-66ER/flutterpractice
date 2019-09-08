9ed0503e317b45f453f1479cb6501eeb5e65f6bb
CyberPunk.B1-66ER <CyberPunk.B1.66ER@gmail.com>
2019-09-07 20:17:46 -0500

    recien me acuerdo de hacer commit a lo q cree desde cero puff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './widgets/listadetransacciones.dart';
+import './models/transaccion.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,51 +13,66 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppHome extends StatelessWidget {
-  final titleControler = TextEditingController();
-  final amountControler = TextEditingController();
+  final List<Transaccion> transa = [
+    Transaccion(
+      id: 't1',
+      title: 'gasto1',
+      amount: 3.5,
+      date: DateTime.now(),
+    ),
+    Transaccion(
+      id: 't2',
+      title: 'gasto2',
+      amount: 6.25,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MoneyPeny'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('Chard!'),
-            ),
-          ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'title'),
-                    controller: titleControler,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'amount'),
-                  ),
-                  FlatButton(
-                    child: Text(
-                      'add Transaccion',                                            
-                    ),
-                    textColor: Colors.purple,
-                    onPressed: () {},
-                  ),
-                ],
+        appBar: AppBar(
+          title: Text('Money Penny'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
               ),
             ),
-          ),
-          ListaTransacciones(),
-        ],
-      ),
-    );
+            Column(
+                children: transa.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 2, color: Colors.purple)),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ${tx.amount}',
+                        style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList()),
+          ],
+        ));
   }
 }
--- lib/models/transaccion.dart
+++ lib/models/transaccion.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/foundation.dart';
+
 class Transaccion {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  Transaccion({
-    @required this.id,
-    @required this.title,
-    @required this.amount,
-    @required this.date,
-  });
+  Transaccion(
+      {@required this.id,
+      @required this.title,
+      @required this.amount,
+      @required this.date, 
+      }
+  );
 }
